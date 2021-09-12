8ccee517a834734066340220273aaf962b2e27c6
vraihxjehxjzhsjd <90188100+vraihxjehxjzhsjd@users.noreply.github.com>
2021-09-12 23:17:14 +0300

    Update _.py

--- _.py
+++ _.py
@@ -3,5 +3,5 @@ from alive_progress import alive_bar
 with alive_bar(20, bar='classic', title='Тест', length=20) as bar:
     count = range(1, 21)
     for count in count:
-        print(f"[{coun}] sended.")
+        print(f"[{count}] sended.")
         bar()
