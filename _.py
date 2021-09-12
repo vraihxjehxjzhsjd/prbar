078329d7b4af25ff3a4851617c6cbb1660253ec3
vraihxjehxjzhsjd <90188100+vraihxjehxjzhsjd@users.noreply.github.com>
2021-09-12 23:28:53 +0300

    Update _.py

--- _.py
+++ _.py
@@ -1,8 +1,8 @@
 from alive_progress import alive_bar
 from time import sleep
 
-with alive_bar(20, title='Тест') as bar:
-    count = range(1, 21)
+with alive_bar(100) as bar:
+    count = range(1, 101)
     for count in count:
         bar()
         print(f"[{count}] sended.")
